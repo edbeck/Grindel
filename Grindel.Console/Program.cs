3f75b845c2e58b2d329ebeb539412a2e0a9fd0d4
edgar beck <edfbeck@gmail.com>
2015-05-16 11:37:14 -0500

    Grindel

---
+++ Grindel.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Grindel.Console
+{
+	class MainClass
+	{
+		private const string usageText = "Hello World";
+		public static void Main (string[] args)
+		{
+			System.Console.WriteLine (usageText);
+		}
+	}
+}
---
+++ Grindel.DataAccess/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Grindel.DataAccess
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
